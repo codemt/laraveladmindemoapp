a083d6970dfbc008b744f0b02bae8a51c9a05ba8
mithileshtarkar <mithilesh.tarkar@ves.ac.in>
2018-11-09 21:39:50 +0530

    Started working on generate PDF module

--- resources/views/admin/invoices/index.blade.php
+++ resources/views/admin/invoices/index.blade.php
@@ -12,17 +12,15 @@
       </div>
       <div class="form-group">
         <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAfBJREFUWAntVk1OwkAUZkoDKza4Utm61iP0AqyIDXahN2BjwiHYGU+gizap4QDuegWN7lyCbMSlCQjU7yO0TOlAi6GwgJc0fT/fzPfmzet0crmD7HsFBAvQbrcrw+Gw5fu+AfOYvgylJ4TwCoVCs1ardYTruqfj8fgV5OUMSVVT93VdP9dAzpVvm5wJHZFbg2LQ2pEYOlZ/oiDvwNcsFoseY4PBwMCrhaeCJyKWZU37KOJcYdi27QdhcuuBIb073BvTNL8ln4NeeR6NRi/wxZKQcGurQs5oNhqLshzVTMBewW/LMU3TTNlO0ieTiStjYhUIyi6DAp0xbEdgTt+LE0aCKQw24U4llsCs4ZRJrYopB6RwqnpA1YQ5NGFZ1YQ41Z5S8IQQdP5laEBRJcD4Vj5DEsW2gE6s6g3d/YP/g+BDnT7GNi2qCjTwGd6riBzHaaCEd3Js01vwCPIbmWBRx1nwAN/1ov+/drgFWIlfKpVukyYihtgkXNp4mABK+1GtVr+SBhJDbBIubVw+Cd/TDgKO2DPiN3YUo6y/nDCNEIsqTKH1en2tcwA9FKEItyDi3aIh8Gl1sRrVnSDzNFDJT1bAy5xpOYGn5fP5JuL95ZjMIn1ya7j5dPGfv0A5eAnpZUY3n5jXcoec5J67D9q+VuAPM47D3XaSeL4AAAAASUVORK5CYII=&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;">
+        <select class="form-control" id="exampleSelect1" name="student_name">
+        </select>
         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
       </div>
       <div class="form-group">
         <label for="exampleSelect1">Select Template </label>
         <select class="form-control" id="exampleSelect1">
-          <option>Fee Reminder </option>
-          <option>New Admission</option>
-          <option>Inquiry Email</option>
-          <option>Class Updates</option>
-          <option>Marketing Email </option>
+          <option>Fee Receipt </option>
+          <option>Attendance </option>
         </select>
       </div>
       <div class="form-group">
@@ -42,6 +40,40 @@
 
               
                   $('.summernote').summernote();
+
+                   $.ajax({
+
+
+                              url: "{{ route('admin.getStudentNames') }}",
+                              method: 'get',
+                              success: function(result){
+
+
+                                console.log(result);
+                              
+                                var trHTML;
+                                $.each(result, function (i, item) {
+                                    
+                                    trHTML += '<option>' + result[i]['name'] + '</option>';
+                                });
+
+                              $('#exampleSelect1').append(trHTML);
+
+
+
+
+                              },
+                              error: function (data) {
+
+                              console.log(data);
+                              console.log('Error:', data);
+
+                              }
+
+                      }); 
+
+
+
           
 
 
